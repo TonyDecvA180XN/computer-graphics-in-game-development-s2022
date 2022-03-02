b7daeae5a28529df9f818de8baa43e4ca7158afa
TonyDecvA180XN <tony.dospiekhov@gmail.com>
2022-03-02 18:48:02 +0300

    Adjust window creation logic

--- src/utils/error_handler.h
+++ src/utils/error_handler.h
@@ -14,3 +14,25 @@
 				.append("\n");                    \
 		throw std::runtime_error(message);        \
 	}
+
+#define WIN_ERROR(text) 													   \
+	{																		   \
+		const DWORD errorId = ::GetLastError();								   \
+		LPWSTR messageBuffer = nullptr;										   \
+																			   \
+		const size_t size = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |	   \
+			FORMAT_MESSAGE_FROM_SYSTEM |									   \
+			FORMAT_MESSAGE_IGNORE_INSERTS,									   \
+			nullptr,														   \
+			errorId,														   \
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),						   \
+			reinterpret_cast<LPWSTR>(&messageBuffer),						   \
+			0,																   \
+			nullptr);														   \
+																			   \
+		std::wstring message(messageBuffer, size);							   \
+		LocalFree(messageBuffer);											   \
+																			   \
+		message.insert(0, text);					   \
+		MessageBox(nullptr, message.c_str(), L"Runtime Failure", MB_ICONERROR);\
+	}
--- src/utils/window.cpp
+++ src/utils/window.cpp
@@ -3,39 +3,81 @@
 #include <windowsx.h>
 
 using namespace cg::utils;
+using namespace std::string_literals;
 
 HWND window::hwnd = nullptr;
 
-
 int cg::utils::window::run(cg::renderer::renderer* renderer, HINSTANCE hinstance, int ncmdshow)
 {
-	// Initialize the window class.
-	WNDCLASSEX window_class = {};
-	window_class.cbSize = sizeof(WNDCLASSEX);
-	window_class.style = CS_HREDRAW | CS_VREDRAW;
-	window_class.lpfnWndProc = window_proc;
-	window_class.hInstance = hinstance;
-	window_class.hCursor = LoadCursor(NULL, IDC_ARROW);
-	window_class.lpszClassName = L"DXSampleClass";
-	RegisterClassEx(&window_class);
+	// Register the window class.
+	LPCWSTR windowClassName = L"DirectX Sample Window Class";
+	LPCWSTR windowName = L"DirectX Sample Window";
 
-	// Create the window and store a handle to it.
-	RECT window_rect = {0, 0, static_cast<LONG>(renderer->get_width()),
-						static_cast<LONG>(renderer->get_height())};
-	AdjustWindowRect(&window_rect, WS_OVERLAPPEDWINDOW, FALSE);
-	hwnd = CreateWindow(
-			window_class.lpszClassName, L"DX12 renderer", WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT, window_rect.right - window_rect.left,
-			window_rect.bottom - window_rect.top, nullptr, nullptr, hinstance, renderer);
+	constexpr DWORD windowStyle = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+
+	WNDCLASS windowClass{};
+
+	windowClass.lpfnWndProc = window_proc;
+	windowClass.hInstance = hinstance;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.style = 0;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	windowClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	windowClass.hbrBackground = static_cast<HBRUSH>(GetStockObject(GRAY_BRUSH));
+	windowClass.lpszMenuName = nullptr;
+
+	if (RegisterClass(&windowClass) == 0)
+	{
+		WIN_ERROR(L"Failed to register a window class: ")
+		return -1;
+	}
+
+	RECT windowBox;
+	windowBox.left = 0;
+	windowBox.top = 0;
+	windowBox.right = static_cast<LONG>(renderer->get_width());
+	windowBox.bottom = static_cast<LONG>(renderer->get_height());
+
+	if (!AdjustWindowRect(&windowBox, windowStyle, false))
+	{
+		WIN_ERROR(L"Failed to adjust window rectangle: ")
+		return -1;
+	}
+
+	// Create the window.
+	HWND hWindow = CreateWindowEx(
+		0, // Optional window styles.
+		windowClassName, // Window class
+		windowName, // Window text
+		windowStyle, // Window style
+		CW_USEDEFAULT, // Position x
+		CW_USEDEFAULT, // Position y
+		windowBox.right - windowBox.left, // Window width
+		windowBox.bottom - windowBox.top, // Window height
+		nullptr, // Parent window
+		nullptr, // Menu
+		hinstance, // Instance handle
+		nullptr // Additional application data
+	);
+
+	if (hWindow == nullptr)
+	{
+		WIN_ERROR(L"Failed to create a window: ")
+		return -1;
+	}
+
+	ShowWindow(hWindow, SW_MAXIMIZE);
 
 	// Initialize the sample. OnInit is defined in each child-implementation of DXSample.
 	renderer->init();
-	ShowWindow(hwnd, ncmdshow);
+
 	// Main sample loop.
 	MSG msg = {};
 	while (msg.message != WM_QUIT)
 	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
@@ -50,51 +92,55 @@ int cg::utils::window::run(cg::renderer::renderer* renderer, HINSTANCE hinstance
 LRESULT cg::utils::window::window_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
 	cg::renderer::renderer* renderer = reinterpret_cast<cg::renderer::renderer*>(
-			GetWindowLongPtr(hwnd, GWLP_USERDATA));
+		GetWindowLongPtr(hwnd, GWLP_USERDATA));
 
 	switch (message)
 	{
-		case WM_CREATE: {
+		case WM_CREATE:
+		{
 			// Save the Renderer* passed in to CreateWindow.
 			LPCREATESTRUCT pCreateStruct = reinterpret_cast<LPCREATESTRUCT>(lparam);
 			SetWindowLongPtr(
-					hwnd, GWLP_USERDATA,
-					reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
-		}
+				hwnd, GWLP_USERDATA,
+				reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
 			return 0;
+		}
 
-		case WM_PAINT: {
+		case WM_PAINT:
+		{
 			if (renderer)
 			{
 				renderer->update();
 				renderer->render();
 			}
-		}
 			return 0;
+		}
 
-		case WM_KEYDOWN: {
+		case WM_KEYDOWN:
+		{
 			if (renderer)
 			{
 				switch (static_cast<UINT8>(wparam))
 				{
-					case 87:// w
+					case 87: // w
 						renderer->move_forward(10.f);
 						break;
-					case 83:// s
+					case 83: // s
 						renderer->move_backward(10.f);
 						break;
-					case 68:// d
+					case 68: // d
 						renderer->move_right(10.f);
 						break;
-					case 65:// a
+					case 65: // a
 						renderer->move_left(10.f);
 						break;
 				}
 			}
-		}
 			return 0;
+		}
 
-		case WM_MOUSEMOVE: {
+		case WM_MOUSEMOVE:
+		{
 			if (renderer)
 			{
 				short x_pos = GET_X_LPARAM(lparam);
@@ -104,12 +150,14 @@ LRESULT cg::utils::window::window_proc(HWND hwnd, UINT message, WPARAM wparam, L
 				renderer->move_yaw((2.f * static_cast<float>(x_pos) / renderer->get_width() - 1.f) * 60.f);
 				renderer->move_pitch((-2.f * static_cast<float>(y_pos) / renderer->get_height() + 1.f) * 60.f);
 			}
-		}
 			return 0;
+		}
 
 		case WM_DESTROY:
+		{
 			PostQuitMessage(0);
 			return 0;
+		}
 	}
 
 	// Handle any messages the switch statement didn't.
